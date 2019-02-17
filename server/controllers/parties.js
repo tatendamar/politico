0e0ad4171fc9deaa5c494e5c18b236519ba9d1ad
tatendamar <tatendamarufu05@gmail.com>
2019-02-17 13:01:21 +0200

    [finishes #164032855] get all parties endpoint

--- server/controllers/parties.js
+++ server/controllers/parties.js
@@ -1,126 +1,15 @@
-import validateParty from '../helpers/validateParty';
-//import check from 'express-validator/check';
-import party from '../models/parties';
+import knex from '../models/knex';
 
-let currentId = party['data'].length;
-
-//get parties
 const getParties = (req, res) => {
-  res.send({ party });
-};
-
-//get a party by id
-const getParty = (req, res) => {
-  const id = req.params.partyId;
-  let found = party['data'].find(party => {
-    return party.id === parseInt(id);
-  });
-
-  if (found) {
-    return res.send({
-      status: party.status,
-      data: found
-    });
-  } else {
-    return res.send({
-      status: 404,
-      message: 'Not Found'
-    });
-  }
-};
-
-//post parties
-const postParty = (req, res) => {
-  const { name, address, email, city, logo } = req.body;
-
-  //let data = req.body;
-  currentId++;
-
-  const newParty = {
-    id: currentId,
-    name: name,
-    email: email,
-    address: address,
-    city: city,
-    logo: logo
-  };
-
-  const err = validateParty(req.body);
-  console.log('JOI Error is', err['error'].details.map(n => console.log(n)));
-
-  let error = err['error'].details.map(n => n.message);
-  for (let i of error) {
-    console.log(i);
-  }
-  console.log(error);
-  if (!name || !email || !address || !city || !logo) {
-    return res.send({
-      status: 400,
-      error: error
-    });
-  }
-
-  party['data'].push(newParty);
-  res.send({
-    status: party.status,
-    data: [
-      {
-        id: newParty.id,
-        name: newParty.name,
-        email: newParty.email,
-        address: newParty.address,
-        city: newParty.city,
-        logo: newParty.logo
-        // dateCreated: day
-      }
-    ]
-  });
-};
-
-//edit parties
-const editParty = (req, res) => {
-  const id = req.params.partyId;
-  let newName = req.body.name;
-
-  let found = party['data'].find(party => {
-    return party.id === parseInt(id);
-  });
-
-  if (found) {
-    found.name = newName;
-    return res.send({
-      status: party.status,
-      data: found
-    });
-  } else {
-    return res.send({
-      status: 404,
-      message: 'Not Found'
-    });
-  }
-};
-
-//delete parties
-const deleteParty = (req, res) => {
-  const id = req.params.partyId;
-
-  let found = false;
-
-  party['data'].forEach((party, index, array) => {
-    if (!found && party.id === parseInt(id)) {
-      array.splice(index, 1);
-    } else {
+  knex
+    .select()
+    .from('parties')
+    .then(parties => {
       res.send({
-        status: 404,
-        message: 'party you want to delete does not exist'
+        status: 200,
+        data: parties
       });
-    }
-  });
-
-  res.send({
-    status: party.status,
-    data: [{ maessage: 'party deleted successfully' }]
-  });
+    });
 };
 
-export default { getParties, getParty, postParty, editParty, deleteParty };
+export default { getParties };
--- server/routes/parties.js
+++ server/routes/parties.js
@@ -1,14 +1,12 @@
 import parties from '../controllers/parties';
 const app = require('express')();
 
-app
-  .route('/parties')
-  .get(parties.getParties)
-  .post(parties.postParty);
-app
-  .route('/parties/:partyId')
-  .get(parties.getParty)
-  .delete(parties.deleteParty);
-app.route('/parties/:partyId/name').put(parties.editParty);
+app.route('/parties').get(parties.getParties);
+//   .post(parties.postParty);
+// app
+//   .route('/parties/:partyId')
+//   .get(parties.getParty)
+//   .delete(parties.deleteParty);
+// app.route('/parties/:partyId/name').put(parties.editParty);
 
 export default app;
